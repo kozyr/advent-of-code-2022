8fa896c3d2719195838661fc5cb3c6e77140b1dd
Sergei Kozyrenko <sergei@parknav.com>
2022-12-03 20:58:11 -0600

    Day 3

---
+++ src/main/kotlin/com/kozyrenko/advent2022/day3-1.kt
@@ -0,0 +1,31 @@
+package com.kozyrenko.advent2022
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    var score = 0
+    File("input/day3.txt").forEachLine { line ->
+        val first = line.substring(0, line.length / 2)
+        val second = line.substring(line.length / 2)
+
+        score += first.toSet().intersect(second.toSet()).sumOf { score(it) }
+
+    }
+    println(score)
+}
+
+fun score(c: Char): Int {
+    return if (c.isLowerCase()) {
+        c - 'a' + 1
+    } else if (c.isUpperCase()) {
+        c - 'A' + 27
+    } else 0
+}
+
+fun String.toSet(): Set<Char> {
+    val unique = mutableSetOf<Char>()
+    for (i in 0 until length) {
+        unique.add(get(i))
+    }
+    return unique
+}
---
+++ src/main/kotlin/com/kozyrenko/advent2022/day3-2.kt
@@ -0,0 +1,23 @@
+package com.kozyrenko.advent2022
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    var score = 0
+    val group = mutableListOf<String>()
+    File("input/day3.txt").forEachLine { line ->
+        group.add(line)
+        if (group.size % 3 == 0) {
+            score += scoreGroup(group)
+            group.clear()
+        }
+    }
+
+    println(score)
+}
+
+fun scoreGroup(group: List<String>): Int {
+    return score(group.map(String::toSet).reduce {
+            acc, set -> acc.intersect(set)
+    }.first())
+}
